42797ace73e2ea03de5a63e564dc1f859fb8d75f
Daniel-Mack <daniel.mack@powerhrg.com>
2018-07-31 08:57:42 -0400

    added link to select a game from the game index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-player1 = Player.create!(name: "Kynsin Viereth", password: "password", skill_level: "high")
-player2 = Player.create!(name: "Xeneth Lofie", password: "words", skill_level: "medium")
-player3 = Player.create!(name: "Kerune Dark", password: "random", skill_level: "low")
+player1 = Player.create!(name: "Kynsin Viereth", password: "password", skill_level: "high", age: 18)
+player2 = Player.create!(name: "Xeneth Lofie", password: "words", skill_level: "medium", age: 34)
+player3 = Player.create!(name: "Kerune Dark", password: "random", skill_level: "low", age: 21)
 
 game1 = Game.create!(name: "Donkey Kung", difficulty_level: "easy", fun_rating: "high")
-game2 = Game.create!(name: "Merio Sisters", difficulty_level: "medium", fun_rating: "medium")
+game2 = Game.create!(name: "Merio Sisters", difficulty_level: "medium", fun_rating: "average")
 game3 = Game.create!(name: "Senic 2", difficulty_level: "hard", fun_rating: "low")
 
 character1 = Character.create!(name: "Bill", player_id: 1, game_id: 1)
