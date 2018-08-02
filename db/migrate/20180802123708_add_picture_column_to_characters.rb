0c0b4a8b5a25dfecf62cdf5ff117e6fdc34e0bf4
Daniel-Mack <daniel.mack@powerhrg.com>
2018-08-02 10:40:57 -0400

    removed migration to add links to pictures

--- db/migrate/20180802123708_add_picture_column_to_characters.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureColumnToCharacters < ActiveRecord::Migration[5.2]
-  def change
-    add_column :characters, :picture, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_123708) do
+ActiveRecord::Schema.define(version: 2018_07_31_130213) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2018_08_02_123708) do
     t.integer "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
   end
 
   create_table "games", force: :cascade do |t|
